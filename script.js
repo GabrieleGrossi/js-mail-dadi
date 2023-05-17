e3a208b6a973d80067d52c6ce9d711b20d5f9106
GabrieleGrossi <grossigabriele98@gmail.com>
2023-05-17 13:20:49 +0200

    add some text and the draw

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const io = Math.floor(Math.random()* (6 - 1 + 1)) + 1
-console.log(io)
+console.log('il mio tiro dei dadi è ' + io)
 const pc = Math.floor(Math.random()* (6 - 1 + 1)) + 1
-console.log(pc)
+console.log('il tiro dei dadi del pc è ' + pc)
 
 if (io > pc) {
-    console.log('Hai vinto')
+    console.log('Hai vinto');
+} else if (io < pc) {
+    console.log('Hai perso, riprovaci');
 } else {
-    console.log('Hai perso')
+    console.log('Hai pareggiato, riprovaci');
 }
